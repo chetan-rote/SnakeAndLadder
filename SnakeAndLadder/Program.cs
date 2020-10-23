5480dcf8da9035dfa339fd47d3f4ed5b1367143d
Chetan <Chetan@DESKTOP-9SU09BS>
2020-10-23 20:43:01 +0530

    Add project files.

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentPosition = 0;
+            Console.WriteLine("Welcome to Snake and Ladder Problem!");            
+            Console.WriteLine("Starting position is : " + currentPosition);
+        }
+    }
+}
